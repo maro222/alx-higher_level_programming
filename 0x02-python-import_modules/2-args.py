d2fd0608e790fb42a8326de50c9a1f0c64745275
omar <omar.a.farouk22>
2023-08-11 13:44:12 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+
+    len = len(sys.argv) - 1
+
+    if len == 0:
+        print("{:d} arguments.".format(0))
+    elif len == 1:
+        print("{:d} argument:".format(1))
+    else:
+        print("{:d} arguments:".format(len))
+        for i in range(1, len + 1):
+            print("{}: {}".format(i, sys.argv[i]))
