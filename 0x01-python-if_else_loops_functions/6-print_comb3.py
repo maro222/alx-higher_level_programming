d77a3a392356a6b78e963bbd20ebccdf6a2b18d8
omar <omar.a.farouk22>
2023-08-10 18:34:31 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i + 1, 10):
-        if i == j + 1:
+        if i != j + 1:
             print("{}{} ,".format(i, j), end='')
 print(f"{i}{i + 1}")
