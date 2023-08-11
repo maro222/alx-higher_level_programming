b451e24a84a45f014808783e649ce165d513ae23
omar <omar.a.farouk22>
2023-08-11 13:48:09 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(len))
         for i in range(1, len + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, sys.argv[i]))
