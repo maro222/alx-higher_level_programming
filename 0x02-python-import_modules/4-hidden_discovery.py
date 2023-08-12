8203e68f3c2e61e01b315c59669c230c7ef396ef
omar <omar.a.farouk22>
2023-08-12 03:32:04 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import hidden_4
 
 if __name__ == "__main__":
     names = dir(hidden_4)
-    for name in range(len(names)):
+    for name in names:
         if name[0] == '_' and name[1] == '_':
             continue
         print("{:s}".format(name))
