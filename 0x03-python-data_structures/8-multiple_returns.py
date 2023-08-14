5690e56b8289e3631e7748e38ab0ef3738236152
omar <omar.a.farouk22>
2023-08-14 15:44:37 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if not sentence or sentence == "":
         mytuple = (0, None)
     else:
-        mytuple = (len(sentence), sentence[i])
+        mytuple = (len(sentence), sentence[0])
     return mytuple
