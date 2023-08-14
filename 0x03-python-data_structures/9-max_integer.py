ba5e538b619d2af08117adf24c81e990afd0f7fd
omar <omar.a.farouk22>
2023-08-14 16:23:57 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3i
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    mmax = my_list[0]
+    for i in range(1, len(my_list)):
+        if mmax < my_list[i]:
+            mmax = my_list[i]
+    print("Max: {:d}".format(mmax))
