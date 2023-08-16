4f85c7d43cb905cbff6763ae58a39c9f33ed720d
omar <omar.a.farouk22>
2023-08-16 15:10:54 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    mmax
+    for k, v in a_dictionary.items():
+        if not mmax or mmax < v:
+            mmax = k
+    return (mmax)
