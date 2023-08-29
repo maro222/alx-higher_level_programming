8aaf9f6809ffacbe5f5f4c5bda67a30fa3925630
omar <omar.a.farouk22>
2023-08-29 06:29:18 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This Module Contain Empty Class"""
+
+
+class Square:
+    """ This is an Empty Class"""
+
+    def __init__(self, size):
+        """ This is initialize magic class"""
+        self.__size = size
