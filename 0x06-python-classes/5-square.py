6d4d9068a2eb7afda5480f9afd1b616ce0b329db
omar <omar.a.farouk22>
2023-08-29 08:00:47 -0700

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" This Module Contain Empty Class"""
+
+
+class Square:
+    """ define square Class of size attribute
+
+        Attributes:
+               size: (int) size of square
+        methosd:
+               __init__: initialize size of square
+    """
+
+    def __init__(self, size=0):
+        """ This is initialize magic class
+
+        Args:
+            size: size of Square instacnce
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """ This function return value of __size
+
+            Return:
+                __size: (int)size of square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """ This function set value of __size
+
+            Args:
+                size: (int)size to be set to __size
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ This Method calculate the area of square
+
+            Return:
+                the Area of square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """this Method print square in # form"""
+        if __size == 0:
+            print()
+            return
+        for i in range(__size):
+            for j in range(__size):
+                print("#")
+            print()
