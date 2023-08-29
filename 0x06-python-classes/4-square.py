6a825f41f4bd619b7e9e8f2f959b0b0e4c313f0c
omar <omar.a.farouk22>
2023-08-29 07:34:27 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -47,4 +47,10 @@ class Square:
             Args:
                 size: (int)size to be set to __size
         """
-        self.__size = size
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
