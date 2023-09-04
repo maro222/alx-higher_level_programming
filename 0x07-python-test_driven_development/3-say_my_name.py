bffcbbebea7b461c5ab01ce3a6cd81eac4b995f7
omar <omar.a.farouk22>
2023-09-04 04:14:27 -0700

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    temp = ""
+    if type(first_name) != type(temp):
+        raise TypeError("first_name must be a string")
+    if type(last_name) != type(temp):
+        raise TypeError("last_name must be a string")
+    print("My name is {}".format(first_name), end='')
+    if last_name != "":
+        print(" {}".format(last_name), end='')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("3-say_my_name.txt")
