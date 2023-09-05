9eb7d3a1e2e4ae31d20572b208b88198cdf5b769
omar <omar.a.farouk22>
2023-09-04 18:43:06 -0700

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" this a module for class Rectangle"""
+
+
+class Rectangle:
+    """ this is an empty Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for width attribute"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """setter for width attribute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height attribute"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """setter for height attribute"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """ calcul the area of rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """ calculthe perimeter of rectangle"""
+        if (self.__height == 0 or self.__width == 0):
+            return 0
+        return ((self.__height + self.__width) * 2)
+
+    def __str__(self):
+        """ magic method str() """
+        for i in range(self.__width):
+            for j in range(self.__height):
+                print('#', end='')
+            print()
