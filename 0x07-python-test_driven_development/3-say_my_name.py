fd525599d8fbe86dd90df9ab6980f6c43051b692
omar <omar.a.farouk22>
2023-09-06 06:55:36 -0700

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,12 +2,13 @@
 def say_my_name(first_name, last_name=""):
     temp = ""
     if type(first_name) != type(temp):
-        raise TypeError("first_name must be a string")
+        raise TypeError("first_name must be a string ")
     if type(last_name) != type(temp):
         raise TypeError("last_name must be a string")
     print("My name is {}".format(first_name), end='')
     if last_name != "":
-        print(" {}".format(last_name), end='')
+        print("{}".format(last_name), end='')
+    print()
 
 
 if __name__ == '__main__':
