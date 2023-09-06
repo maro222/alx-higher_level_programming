c38d7e4971ed5c3e1260dfc2cd522efa59897ff8
omar <omar.a.farouk22>
2023-09-05 19:52:45 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.x = getattr(magic_string, x, -1) + 1
+    magic_string.x = getattr(magic_string, 'x', -1) + 1
     return "BestSchool" + ", BestSchool" * magic_string.x
