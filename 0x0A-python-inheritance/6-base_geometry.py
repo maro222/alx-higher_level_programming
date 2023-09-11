70c3710004fc316b7de21db2144581963c50a432
omar <omar.a.farouk22>
2023-09-11 07:04:52 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for task 6"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
