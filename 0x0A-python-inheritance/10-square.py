1091ee7069df76e12dbf795951b9ba82ab73d335
omar <omar.a.farouk22>
2023-09-11 10:01:22 -0700

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for task 10"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        super(size, size)
+        self.__size = size
+
+    def area():
+        """ area implementation"""
+        return (self.__size * self.__size)
