4fb8d52b2ad1ccaa2672b8b628dc43dd26f8866e
omar <omar.a.farouk22>
2023-09-11 04:53:50 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for task 0"""
+
+
+def lookup(obj):
+    """a function that returns the list of available
+       attributes and methods of an object"""
+    return (dir(obj))
