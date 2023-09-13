16e1083384a27c374caa965bc08da4caa7145bbf
omar <omar.a.farouk22>
2023-09-12 17:24:38 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for task 3"""
+import json
+
+
+def to_json_string(my_obj):
+    """a function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
