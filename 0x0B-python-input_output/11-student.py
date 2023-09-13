877c2fdcde94ac86d1c6669c7a442de205c22126
omar <omar.a.farouk22>
2023-09-13 16:40:11 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for task_9"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        res = {}
+        res = self.__dict__.copy()
+        if attrs is None:
+            return res
+        keys = list(res.keys())
+        for key in keys:
+            if key not in attrs:
+                del res[key]
+        return res
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
