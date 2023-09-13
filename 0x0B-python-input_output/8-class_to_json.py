f7e462fc2f83032707dea104c1559ad69cbdfb21
omar <omar.a.farouk22>
2023-09-13 11:43:10 -0700

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for task_8"""
+
+
+def class_to_json(obj):
+    """
+     a function that returns the dictionary description with
+     simple data structure (list, dictionary, string, integer and boolean)
+     for JSON serialization of an object
+    """
+    dic = {}
+    if __dict__ in dir(obj):
+        dic = obj.__dict__.copy()
+    return dic
