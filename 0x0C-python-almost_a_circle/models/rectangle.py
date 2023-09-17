ded90635c12bdff79bf596a46cdf583b7584dbc1
maro222 <omar.a.farouk22>
2023-09-16 19:13:09 -0700

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Module for task 2"""
+
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiation of Rectangle class"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super.__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        self.__width = width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """height setter"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """x setter"""
+        self.__x = x
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """y setter"""
+        self.__y = y
