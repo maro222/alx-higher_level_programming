a63ebe130273d167f421a173b47441fdcf4c1a3b
maro222 <omar.a.farouk22>
2023-09-19 15:48:55 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -120,3 +120,12 @@ class Rectangle(Base):
                     self.x = value
                 else:
                     self.y = value
+
+    def to_dictionary(self):
+        """that returns the dictionary representation of a Rectangle"""
+        return {"id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+                }
