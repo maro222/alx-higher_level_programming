0bcf9d43d126943a4f1d810bf3473b070dfb4ba1
omar <omar.a.farouk22>
2023-10-18 16:32:05 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1' IF NOT EXISTS
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
