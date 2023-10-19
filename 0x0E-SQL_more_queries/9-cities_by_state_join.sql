31fbd6d4e2680dc65950046a124dddc01245f1fd
omar <omar.a.farouk22>
2023-10-18 18:43:57 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 SELECT `cities`.`id`, `cities`.`name`, `state`.`name`
 FROM `cities`
 INNER JOIN `states`
-ON `state_id` = `states`.`id`
-ORDER BY `cities`.`id`
+ON `cities`.`state_id` = `states`.`id`
+ORDER BY `cities`.`id` ASC;
