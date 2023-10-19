8e1873735fd3bc101c0a0dc5d8d15ef57caa3dad
omar <omar.a.farouk22>
2023-10-18 19:18:07 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT t.`title`, tg.`genre_id`
 FROM `tv_shows` AS t
-INNER JOIN `tv_shows_genres` AS tg
+INNER JOIN `tv_show_genres` AS tg
 ON t.`id` = tg.`show_id`
 ORDER BY t.`title`, tg.`genre_id`;
