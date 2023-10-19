f9e0289ab6d19f5d7700fd9e800ed37f6ba9f731
omar <omar.a.farouk22>
2023-10-18 18:40:50 -0700

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+SELECT `cities`.`id`, `citites`.`name`, `state`.`name`
+FROM `cities`
+INNER JOIN `states`
+ON `state_id` = `states`.`id`
+ORDER BY `cities`.`id`
